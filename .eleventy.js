4c179460bf8c772464274d67d43a0543cb6b7e30
Philip Allfrey <philip@allfrey.co.nz>
2020-06-22 21:05:26 +1200

    Tweak site, commit package/lock, add date filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
-module.exports = {
-  dir: {
-    data: 'data',
-    includes: 'includes',
-    input: 'src',
-    output: 'dist'
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addFilter("date", function(value) {
+    return new Date(value)
+      .toUTCString()
+      .replace(/[^,]+, /,'') //Strip day of week
+      .replace(/:\d{2} GMT/, ' UTC'); //strip seconds
+  });
+
+  return {
+    dir: {
+      data: 'data',
+      includes: 'includes',
+      input: 'src',
+      output: 'dist'
+    }
   }
+
 }
