c2acc3ab7dc4368a84f4d007c049877e7d54699e
Philip Allfrey <philip@allfrey.co.nz>
2020-06-29 21:10:27 +1200

    Add zero-width space after / in repo name to fit long names better

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,10 @@ module.exports = function(eleventyConfig) {
       .replace(/:\d{2} GMT/, ' UTC'); //strip seconds
   });
 
+  eleventyConfig.addFilter("splitAtSlash", function(value) {
+    return value.replace('/', '/&#8203;'); //Add zero-width space after / in repo name
+  });
+
   return {
     dir: {
       data: 'data',
