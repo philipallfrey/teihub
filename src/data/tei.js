cbd310dacbbf9d1732b552fb153a42f1aed202b6
Philip Allfrey <philip@allfrey.co.nz>
2021-02-26 13:46:08 +1300

    Use flat-file database instead of Fauna

--- src/data/tei.js
+++ src/data/tei.js
@@ -1,11 +1,5 @@
-const latest = require('./latest');
+const repos = require('../../data/teihub.json');
 const fs = require('fs');
-const { parseAsync } = require('json2csv');
-
-let faunadb = require('faunadb'),
-q = faunadb.query;
-
-let client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 
 // Human-readable filesize function taken from https://github.com/hustcc/filesize.js/
 function filesize(bytes, fixed) {
@@ -23,123 +17,7 @@ function filesize(bytes, fixed) {
   return `${bytes.toFixed(fixed)} ${unit[loop]}`;
 }
 
-async function updateDatabase(){
-  //Get aggregate data from database
-  let allAggregates = await client.query(
-    q.Map(
-      q.Paginate(q.Match(q.Index("all_aggregates"))),
-      q.Lambda("x", q.Get(q.Var("x")))
-    )
-  )
-  .catch(err => {
-    console.log(err);
-    return;
-  });
-
-  //Put aggregate data into a Map, so it's easy to query by first letter
-  const aggregates = new Map();
-  allAggregates.data.forEach(item => {
-    aggregates.set(item.data.key, item.data.values);
-  });
-
-  //Now put latest data into database, and update aggregates
-  for(let repo of latest) {
-    //Get first letter of repo name
-    const aggregateKey = repo.name.slice(0,1);
-
-    //We have aggregate data for this letter
-    if(aggregates.has(aggregateKey)){
-      let aggregateData = aggregates.get(aggregateKey);
-      if( aggregateData[repo.name] !== undefined){
-        //get full data from DB
-        const {data: repoDataResults} = await client.query(
-          q.Map(
-            q.Paginate(q.Match(q.Index('repos_search_by_name'), repo.name)),
-            q.Lambda("x", q.Get(q.Var("x")))
-          )
-        ).catch( err => {
-          //Doesn't exist in DB
-        });
-        if(repoDataResults.length > 0){
-          //merge lists of files
-          const filesFromDB = repoDataResults[0].data.files;
-          repo.files = [...new Set([...filesFromDB,...repo.files])];
-          //merge lists of languages
-          const langsFromDB = repoDataResults[0].data.langs || [];
-          repo.langs = [...new Set([...langsFromDB,...repo.langs])];
-          //use most recent date (in case of backfilled data)
-          repo.date = repoDataResults[0].data > repo.date ? repoDataResults[0].data : repo.date;
-          //save back to DB
-          await client.query(
-            q.Replace(repoDataResults[0].ref, {data: repo})
-          ).catch(err => console.log(err));
-        } else {
-          await client.query(
-            q.Create('repos', {data: repo})
-          ).catch(err => console.log(err));
-        }
-        //update count for aggregate
-        repo.count = repo.files.length;
-        delete repo.files;
-        aggregateData[repo.name] = repo;
-        aggregates.set(aggregateKey, aggregateData);
-      } else {
-        //No existing aggregate data for this repo name
-        //This also implies the current repo is not in the DB
-        //Just set both of them
-        await client.query(
-          q.Create('repos', {data: repo})
-        ).catch(err => console.log(err));
-
-        repo.count = repo.files.length;
-        delete repo.files;
-        aggregateData[repo.name] = repo;
-        aggregates.set(aggregateKey, aggregateData);
-      }
-    } else {
-      //No existing aggregate data for this letter
-      //This also implies the current repo is not in the DB
-      //Set them both
-      await client.query(
-        q.Create('repos', {data: repo})
-      );
-
-      repo.count = repo.files.length;
-      delete repo.files;
-      let aggregateData = {};
-      aggregateData[repo.name] = repo;
-      aggregates.set(aggregateKey, aggregateData);
-    }
-  }
-
-  //Now put aggregates back into database
-  let aggregatesArray = [];
-  aggregates.forEach( async (values, key) => {
-    aggregatesArray = [...aggregatesArray, ...Object.values(values)];
-    aggregatesArray.push(...Array.from(values));
-    const result = await client.query(
-      q.Paginate(q.Match(q.Index('aggregates_search_by_key'), key))
-    ).catch(async (err) => {
-      console.log(err);
-    });
-    if(result !== undefined && result.data.length > 0){
-      //Aggregate exists, replace old version
-      await client.query(
-        q.Replace(result.data[0], {data: {key:key, values:values}})
-      ).catch(err => console.log(err))
-    } else {
-      //Aggregate doesn't exist, create it
-      await client.query(
-        q.Create( 'aggregates', {data: {key: key, values: values}})
-      ).catch(err => console.log(err))
-    }
-  });
-
-  return aggregatesArray;
-}
-
 module.exports = async function() {
-  const repos = await updateDatabase();
   const repoCount = repos.length;
   const docCount = repos.reduce( (sum, current) => {
     return sum + current.count;
@@ -189,28 +67,6 @@ module.exports = async function() {
   const lastIndexed = [...repos].sort((a, b) => b.date - a.date);
   const matches = [...repos].sort((a, b) => b.count - a.count);
   const repository = [...repos].sort((a, b) => a.name.localeCompare(b.name));
-
-  //Write full dataset to JSON file, to allow people to download
-  const repoString = JSON.stringify(repos);
-  const distPath = __dirname + '/../../dist';
-  if(!fs.existsSync(distPath)) fs.mkdirSync(distPath);
-  fs.writeFileSync(distPath + '/teihub.json', repoString);
-
-  //Also write full dataset to CSV file
-  const fields = ['date', 'name', 'url', 'desc', 'langs', 'count'];
-  const opts = { fields };
-
-  const reposWithLangString = [];
-  for(let current of repos){
-    const newEntry = Object.assign( {}, current);
-    newEntry.langs = (current.langs) ? current.langs.join('|') : '';
-    reposWithLangString.push(newEntry);
-  };
-
-  const csv = await parseAsync(reposWithLangString, opts)
-    .catch(err => console.error(err));
-  fs.writeFileSync(distPath + '/teihub.csv', csv);
-
 
   return {
     csvSize: filesize(csv.length, 0),
