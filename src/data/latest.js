d632d997e5af08eac3b28ded17ad560202f56604
GitHub Action <action@github.com>
2022-03-09 08:41:19 +0000

    Update results 4

--- src/data/latest.js
+++ src/data/latest.js
@@ -1,1 +1,1 @@
-module.exports = [];
+module.exports = [{"date":1646815275999,"name":"erc-dharma/tfd-nusantara-philology","url":"https://github.com/erc-dharma/tfd-nusantara-philology","desc":"DHARMA project, task force D","files":["output/critical-edition/editedXML/EDITED_DHARMA_CritEdKakavinParthayajna.xml","editions/DHARMA_CritEdKakavinParthayajna.xml"],"langs":["san","kaw"]}];
