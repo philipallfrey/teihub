1f265c17a6bc0db0cfe50e7ca957d356e8b77517
GitHub Action <action@github.com>
2022-05-12 18:53:46 +0000

    Update results [skip ci]

--- src/data/latest.js
+++ src/data/latest.js
@@ -1,1 +1,1 @@
-module.exports = [];
+module.exports = [{"date":1652381622999,"name":"FiscalEstate/Fiscus","url":"https://github.com/FiscalEstate/Fiscus","desc":"Fiscal Estate in Medieval Italy: Continuity and Change (9th – 12th centuries)","files":["webapps/ROOT/content/xml/epidoc/doc2491.xml"],"langs":[]}];
