6a378bdf8cb423ffb58238b2f050c569f80d2925
GitHub Action <action@github.com>
2022-12-05 05:43:53 +0000

    Update results [skip ci]

--- src/data/latest.js
+++ src/data/latest.js
@@ -1,1 +1,1 @@
-module.exports = [{"date":1670215712999,"name":"Project-Cairo-Urban-News/CairoUrbanNews","url":"https://github.com/Project-Cairo-Urban-News/CairoUrbanNews","desc":"Al-Waqa'i' al-Misriyya Urban News","files":["articles/arabic/1282.xml"],"langs":["ara","eng","ota"]}];
+module.exports = [{"date":1670219030999,"name":"Project-Cairo-Urban-News/CairoUrbanNews","url":"https://github.com/Project-Cairo-Urban-News/CairoUrbanNews","desc":"Al-Waqa'i' al-Misriyya Urban News","files":["articles/arabic/1282.xml"],"langs":["ara","eng","ota"]}];
