2452ba3e94678024f96dd6d629a6deb19ccc4398
GitHub Action <action@github.com>
2023-03-09 10:48:49 +0000

    Update results [skip ci]

--- src/data/latest.js
+++ src/data/latest.js
@@ -1,1 +1,1 @@
-module.exports = [{"date":1678355312999,"name":"papyri/idp.data","url":"https://github.com/papyri/idp.data","desc":"Data from the Integrating Digital Papyrology project","files":["DCLP/60/59959.xml"],"langs":["eng","grc"]}];
+module.exports = [];
